712d403c95c346d3e78f01af003911ab7d8d9e42
Mario Paonessa <mario.paonessa@swisscom.com>
2017-10-22 15:21:47 +0200

    schwizerdütschebot

--- swissgermanbot.py
+++ swissgermanbot.py
@@ -2,8 +2,22 @@
 from chatterbot.trainers import ListTrainer
 from chatterbot import ChatBot
 
+chatbot = ChatBot("Peter Müller",
+                  preprocessors=['chatterbot.preprocessors.clean_whitespace'],
+                  storage_adapter="chatterbot.storage.MongoDatabaseAdapter",
+                  logic_adapters=[
+                      {
+                          'import_path': 'chatterbot.logic.BestMatch'
+                      },
+                      {
+                          'import_path': 'chatterbot.logic.LowConfidenceAdapter',
+                          'threshold': 0.65,
+                          'default_response': 'Sorry ich ha di ned verstande'
+                      }
+                  ],
+                  trainer='chatterbot.trainers.ListTrainer'
+                  )
 
-chatbot = ChatBot("Peter Müller")
 conversation = [
     "Hoi",
     "Salü",
@@ -17,7 +31,5 @@ conversation = [
 chatbot.set_trainer(ListTrainer)
 chatbot.train(conversation)
 
-
-
-response = chatbot.get_response("Wie gohts der")
+response = chatbot.get_response("Wie gdohts der")
 print(response)
